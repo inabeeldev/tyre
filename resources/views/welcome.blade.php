2c50e7ba67a62f1a9097a6419299161be36e6e55
nabeeldevs <ahmed.nab33l@gmail.com>
2024-01-06 01:56:03 +0500

    up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,6 +31,34 @@
 
     <!-- Template Stylesheet -->
     <link href="{{ asset('public/tyre/css/style.css') }}" rel="stylesheet">
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11452753327"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'AW-11452753327');
+    </script>
+    <!-- Event snippet for Phone Calls OTS conversion page In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
+    <script>
+        function gtag_report_conversion(url) {
+            var callback = function () {
+                if (typeof(url) !== 'undefined') {
+                    window.location = url;
+                }
+            };
+
+            gtag('event', 'conversion', {
+                'send_to': 'AW-11452753327/u656CKbD6IIZEK_LjNUq',
+                'event_callback': callback
+            });
+
+            return false;
+        }
+    </script>
 </head>
 
 <body>
