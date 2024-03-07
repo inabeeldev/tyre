7045b00bec0b977054f0a8c957119cd4652a23a6
nabeeldevs <ahmad.nab331@gmail.com>
2024-03-08 01:55:17 +0500

    up

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -6,28 +6,31 @@ echo '<?xml version="1.0" encoding="utf-8"?>';
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
             http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
 
 
 <url>
-  <loc>https://onlinetireshop.ae/</loc>
-  <lastmod>2024-01-04T20:27:04+00:00</lastmod>
+  <loc>https://www.onlinetireshop.ae/</loc>
+  <lastmod>2024-03-07T13:31:58+00:00</lastmod>
   <priority>1.00</priority>
 </url>
 <url>
-  <loc>https://onlinetireshop.ae/about</loc>
-  <lastmod>2024-01-04T20:27:04+00:00</lastmod>
+  <loc>https://www.onlinetireshop.ae/about-us</loc>
+  <lastmod>2024-03-07T13:31:58+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
-  <loc>https://onlinetireshop.ae/services</loc>
-  <lastmod>2024-01-04T20:27:04+00:00</lastmod>
+  <loc>https://www.onlinetireshop.ae/tyre-service-dubai</loc>
+  <lastmod>2024-03-07T13:31:58+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
-  <loc>https://onlinetireshop.ae/contact</loc>
-  <lastmod>2024-01-04T20:27:04+00:00</lastmod>
+  <loc>https://www.onlinetireshop.ae/contact-us</loc>
+  <lastmod>2024-03-07T13:31:58+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 
 
 </urlset>
+
+
